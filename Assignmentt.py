81b8644e3397047b0e4a63a9658afb81adf1e2dd
githubpiyush <piyush.jain119981@gmail.com>
2021-06-08 23:22:19 +0530

    RDBMS stage 1

--- Assignmentt.py
+++ Assignmentt.py
@@ -1,15 +1,17 @@
 from create_table import create_table
 import re
+from mysql.connector import Error
 
 def check_date_format(DOB):
     match = re.match("^(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[012])\d{4}$", DOB)
     return bool(match)
 
 def read_file(filename) :
+
     line_counter = 0
-    column_name = []
+    column_name_list = []
     file_object = open(filename, "r")
-    column_names = ""
+    column_name_string = ""
 
     for current_row in file_object :
         line_counter += 1
@@ -20,10 +22,12 @@ def read_file(filename) :
 
         if line_counter == 1:
             for index in current_row :
-                column_name.append(index)
-                column_names = column_names + index + ","
-            country_index = column_name.index('Country')
-            DOB_index = column_name.index('DOB')
+                column_name_list.append(index)
+                column_name_string = column_name_string + index + ","
+
+            country_index = column_name_list.index('Country')
+            DOB_index = column_name_list.index('DOB')
+
         else:
             table_name = current_row[country_index]
 
@@ -32,9 +36,12 @@ def read_file(filename) :
                 current_row[DOB_index] = DOB[4:] + DOB[2:4] + DOB[0:2]
 
             connection, database_cursor = create_table(table_name)
-            insert_table_query = 'INSERT INTO Table_'+ table_name +' ('+column_names[:-1]+ ') VALUES '+ str(tuple(current_row))
-            database_cursor.execute(insert_table_query)
-            connection.commit()
-
+            try:
+                insert_table_query = 'INSERT INTO Table_'+ table_name +' ('+column_name_string[:-1]+ ') VALUES '+ str(tuple(current_row))
+                database_cursor.execute(insert_table_query)
+                connection.commit()
+            except Error as e:
+                # print('Error occured :: ', e)
+                pass
 if __name__ == '__main__':
     read_file('sample.txt')
