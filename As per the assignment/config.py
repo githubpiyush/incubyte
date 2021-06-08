015d9e09e2ec1784872c2deca6a727f455f550e5
githubpiyush <piyush.jain119981@gmail.com>
2021-06-09 02:01:04 +0530

    Final push after cleaning

--- As per the assignment/config.py
+++
@@ -1,2 +0,0 @@
-mongo_url = "mongodb://localhost:27017/"
-db_name = 'local'
--- As per the assignment/create_table.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-from config import mongo_url
-
-def create_collection_obj(db_name, coll_name):
-    myclient = pymongo.MongoClient(mongo_url)
-    mydb = myclient[db_name]
-    mycol = mydb[coll_name]
-    mycol.create_index("Customer_Name", unique=True)
-    return mycol
